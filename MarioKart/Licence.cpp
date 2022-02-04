94335e1df51e6a529e5c75e0295277080f22f739
c00262339 <c00262339@itcarlow.ie>
2022-02-04 13:17:13 +0000

    Created initialize in licence.cpp ~Leo

--- MarioKart/Licence.cpp
+++ MarioKart/Licence.cpp
@@ -2,6 +2,7 @@
 #include "Licence.h"
 #include "Game.h"
 
+// Creates license page and will move to splash screen after a few seconds.
 
 Licence::Licence()
 {
@@ -10,4 +11,16 @@ Licence::Licence()
 
 Licence::~Licence()
 {
+}
+
+void Licence::initialise(sf::Font& t_font)
+{
+	m_font = t_font;
+	m_message.setFont(m_font);
+	m_message.setString("Created by Sine Whooley and Leo Carabini");
+
+	m_message.setFillColor(sf::Color::White);
+	m_message.setCharacterSize(36);
+	m_message.setPosition(0.0f, 70.0f);
+	m_cumulativeTime = sf::seconds(0);
 }

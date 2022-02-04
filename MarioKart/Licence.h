374126d275cf2c4256aafa50429bcf0d755afe91
c00262339 <c00262339@itcarlow.ie>
2022-02-04 13:06:12 +0000

    Added code for licence.h ~Leo

--- MarioKart/Licence.h
+++ MarioKart/Licence.h
@@ -4,11 +4,11 @@
 #include <SFML\Graphics.hpp>
 
 /// <summary>
-/// @author Peter Lowe
-/// @version 1.0
-/// @date may 2016
+/// Name: Leo Carabini
+/// ID: C00262339
+/// Date: 4th February 2022
 /// 
-/// \brief simple page to display Licence text for fixed period.
+/// Code to display licence text.
 /// 
 /// </summary>
 class Licence
@@ -17,7 +17,14 @@ public:
 	Licence();
 	~Licence();	
 
+	void initialise(sf::Font& t_font); 
+	void render(sf::RenderWindow& t_window);
+	void update(sf::Time t_deltaTime); 
+
 protected:
+	sf::Font m_font;
+	sf::Text m_message; 
+	sf::Time m_cumulativeTime;
 
 };
 
